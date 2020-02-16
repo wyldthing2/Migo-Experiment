b33d5eb28f56617ff84af735fd2e90703023822b
Wyldthing2 <xyzzynot2@yahoo.com>
2020-02-15 22:13:55 -0700

    Before Adding Toon Project

---
+++ Migo Experiment Unity/Assets/Scripts/InputToMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputToMovement : MonoBehaviour
+{
+    public CharacterController Controller;
+    public float Speed = .12f;
+    public Transform PlayerObject;
+    public float TurnSpeed = 2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * y;
+
+        Controller.Move(move * Speed);
+
+        PlayerObject.Rotate(Vector3.up * x);
+
+    }
+}
